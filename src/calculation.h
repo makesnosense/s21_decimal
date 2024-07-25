4240e00c186eca839a0346fe4f6a9d6163a95230
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2024-07-25 21:00:37 +0300

    bug: removed get_scale_difference_from_decimals from arithmetic.h

--- src/calculation.h
+++ src/calculation.h
@@ -2,6 +2,3 @@ typedef enum CalculationResult {
   OK = 0,
   CONVERSION_ERROR = 1,
 } CalculationResult;
-
-int get_scale_difference_from_decimals(s21_decimal decimal_1,
-                                       s21_decimal decimal_2);
