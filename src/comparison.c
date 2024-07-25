f4da62aa89e36c67b2971a7f5364f209052cdb01
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2024-07-25 20:43:47 +0300

    refactor: refactored s21_is_equal

--- src/comparison.c
+++ src/comparison.c
@@ -12,32 +12,32 @@ int s21_is_equal(s21_decimal decimal_1, s21_decimal decimal_2) {
   if (get_sign(decimal_1) != get_sign(decimal_2)) {
     bool decimals_are_equal = false;
   } else {
-    int decimal_1_scale = get_scale(decimal_1.bits[3]);
-    int decimal_2_scale = get_scale(decimal_2.bits[3]);
-
-    debug_printf("\n%d %d хххууу\n", decimal_1_scale, decimal_2_scale);
-
-    int scale_difference = 0;
-
-    if (decimal_1_scale > decimal_2_scale) {
-      scale_difference = decimal_1_scale - decimal_2_scale;
-      multiply_mantissas(decimal_2.bits,
-                         get_mantissa_with_power_of_ten(scale_difference),
-                         normalized_mantissa_2);
-
+    if (get_scale(decimal_1.bits[3]) == get_scale(decimal_2.bits[3])) {
       mantissa3_to_mantissa6(decimal_1.bits, normalized_mantissa_1);
+      mantissa3_to_mantissa6(decimal_2.bits, normalized_mantissa_2);
+    } else {
+      s21_two_decimals two_decimals_sorted_ascending_by_scale =
+          sort_decimals_ascending_by_scale(decimal_1, decimal_2);
 
-    } else if (decimal_1_scale < decimal_2_scale) {
-      scale_difference = decimal_2_scale - decimal_1_scale;
-      multiply_mantissas(decimal_1.bits,
+      s21_decimal smaller_scale_decimal =
+          two_decimals_sorted_ascending_by_scale.decimals[0];
+
+      s21_decimal bigger_scale_decimal =
+          two_decimals_sorted_ascending_by_scale.decimals[1];
+
+      int smaller_scale = get_scale(smaller_scale_decimal.bits[3]);
+      int bigger_scale = get_scale(bigger_scale_decimal.bits[3]);
+
+      debug_printf("\n%d %d \n", smaller_scale, bigger_scale);
+
+      int scale_difference = 0;
+
+      scale_difference = bigger_scale - smaller_scale;
+      multiply_mantissas(smaller_scale_decimal.bits,
                          get_mantissa_with_power_of_ten(scale_difference),
                          normalized_mantissa_1);
 
-      mantissa3_to_mantissa6(decimal_2.bits, normalized_mantissa_2);
-
-    } else {
-      mantissa3_to_mantissa6(decimal_1.bits, normalized_mantissa_1);
-      mantissa3_to_mantissa6(decimal_2.bits, normalized_mantissa_2);
+      mantissa3_to_mantissa6(bigger_scale_decimal.bits, normalized_mantissa_2);
     }
 
     debug_print_mantissa_as_binary(normalized_mantissa_1, 6);
