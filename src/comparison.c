99b6d64f7ddf9018a8f80126e9dae96f04a1280e
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2024-08-04 17:19:48 +0300

    refactor: is_eaul with casting_decimals_to_normalized_mantissa

--- src/comparison.c
+++ src/comparison.c
@@ -161,35 +161,8 @@ int s21_is_equal(s21_decimal decimal_1, s21_decimal decimal_2) {
   } else if (get_sign(decimal_1) != get_sign(decimal_2)) {
     decimals_are_equal = false;
   } else {
-    int scale_difference =
-        get_scale_difference_from_decimals(decimal_1, decimal_2);
-
-    if (scale_difference == 0) {
-      mantissa3_to_mantissa6(decimal_1.bits, normalized_mantissa_1);
-      mantissa3_to_mantissa6(decimal_2.bits, normalized_mantissa_2);
-    } else {
-      s21_two_decimals two_decimals_sorted_ascending_by_scale =
-          sort_decimals_ascending_by_scale(decimal_1, decimal_2);
-
-      s21_decimal smaller_scale_decimal =
-          two_decimals_sorted_ascending_by_scale.decimals[0];
-
-      s21_decimal bigger_scale_decimal =
-          two_decimals_sorted_ascending_by_scale.decimals[1];
-
-      multiply_mantissas(smaller_scale_decimal.bits,
-                         get_mantissa_with_power_of_ten(scale_difference),
-                         normalized_mantissa_1);
-
-      mantissa3_to_mantissa6(bigger_scale_decimal.bits, normalized_mantissa_2);
-
-      // debug_printf("\n%d %d \n", get_scale(smaller_scale_decimal.bits[3]),
-      //              get_scale(bigger_scale_decimal.bits[3]));
-    }
-
-    // debug_print_mantissa_as_binary(normalized_mantissa_1, 6);
-    // debug_print_mantissa_as_binary(normalized_mantissa_2, 6);
-
+    casting_decimals_to_normalized_mantissa(decimal_1, normalized_mantissa_1,
+                                            decimal_2, normalized_mantissa_2);
     for (int i = 0; i < 6 && decimals_are_equal == true; i++) {
       if (normalized_mantissa_1[i] != normalized_mantissa_2[i]) {
         decimals_are_equal = false;
